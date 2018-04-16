75a456e661a14ec4837192b70bc8d7e26a82486f
Shruti Pasrija <spasrija@wario.(none)>
2018-04-16 02:03:46 -0500

    Added a sequence but having compile errors

--- uvm_tb_fifo/input_sequence.sv
+++ uvm_tb_fifo/input_sequence.sv
@@ -1,3 +1,8 @@
+`include "uvm_macros.svh"
+`include "input_tx.sv"
+
+//package sequences;
+import uvm_pkg::*;
 class input_sequence extends uvm_sequence #(input_tx);
 	`uvm_object_utils(input_sequence);
 
@@ -13,7 +18,14 @@ class input_sequence extends uvm_sequence #(input_tx);
 
 		//Generate Transactions
 
+		//trans 1
+
+		assert(std::randomize(tx) with {in_tx.rd == 1'b1; in_tx.wr == 1'b1;});
+
+
 		//
 		finish_item(in_tx);
 	endtask : body
-endclass
+endclass:input_sequence
+
+//endpackage: sequences
