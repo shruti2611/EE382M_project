6a5d05f40843b2793506b73d786805a80b2447f4
Shruti Pasrija <spasrija@wario.(none)>
2018-04-21 14:12:45 -0500

    Adding coverage for basic fifo

--- tb/coverage.sv
+++ tb/coverage.sv
@@ -20,6 +20,59 @@ class coverage extends uvm_component;
 	logic [31:0] data_out;
 	logic fifo_empty;
 	logic fifo_full;
+
+	
+	 covergroup inputs;
+        
+    	 read: coverpoint rd {
+		bins read_low = 1'b0;
+		bins read_high = 1'b1;
+
+	 }
+
+	 write: coverpoint wr {
+		bins write_low = 1'b0;
+		bins write_high = 1'b1;
+
+	 }
+
+	 empty: coverpoint fifo_empty{
+		bins not_empty = 1'b0;
+		bins empty = 1'b1;
+	 } 
+
+	 full: coverpoint fifo_full{
+		bins not_full = 1'b0;
+		bins full = 1'b1;
+	 }
+
+	 read_empty: cross coverpoint fifo_empty,
+			   coverpoint rd;
+
+
+	 write_full: cross coverpoint fifo_full,
+			   coverpoint wr;
+
+	 empty_not_full: coverpoint (fifo_empty && !fifo_full) {
+			bins true = 1'b1;
+			bins false = 1'b0;
+	 }
+
+	 full_not_empty: coverpoint (fifo_full && !fifo_empty) {
+			bins true = 1'b1;
+			bins false = 1'b0;
+	 }
+
+	 reset: coverpoint(rst && !fifo_full && !fifo_empty){
+
+			bins true = 1'b1;
+			bins false = 1'b0;
+
+	 }
+
+
+
+
 	
 	function new(string name, uvm_component parent);
 		super.new(name, parent);
