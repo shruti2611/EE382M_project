c070dd93bed1823969f5ad8b77bdcc76b322fb28
shruti2611 <spasrija@wario.(none)>
2018-05-06 17:20:22 -0500

    Removing bkup

--- bkp/uvm_tb_fifo/input_interface.sv
+++
@@ -1,5 +0,0 @@
-interface mesi_input_interface(input clk, input rst);
-	logic wr;
-	logic rd;
-	logic [31:0] data_in;
-endinterface : mesi_input_interface
--- bkp/uvm_tb_fifo/output_interface.sv
+++
@@ -1,5 +0,0 @@
-interface mesi_output_interface(input clk, input rst);
-	logic [31:0] data_out;
-	logic status_empty;
-	logic status_full;
-endinterface : mesi_output_interface
